2f6b97ae72e516ea280873275b45d115e933f65f
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-16 16:01:24 -0600

    created a new pattern

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence(){
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+}
+
+
+
+
+
+
 //alert("you have successfully linked game.js to index.html!");
+
+//Confirmation of linking jQuery to website:
+//$("h1");
+//S.fn.init [h1#level-title, prevObject: S.fn.init(1)]
