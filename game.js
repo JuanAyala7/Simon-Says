d52b1b16397c4fa89f5393beaaf33c55d8b56929
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-18 16:20:05 -0600

    adding arrays, audio and animation

--- game.js
+++ game.js
@@ -2,11 +2,23 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
-function nextSequence(){
+var randomChosenColour = buttonColours[randomNumber];
+
+
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-
-  var randomChosenColor = buttonColors[randomNumber];
-
   gamePattern.push(randomChosenColour);
+  
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
+
+
+//alert("you have successfully linked game.js to index.html!");
+
+//Confirmation of linking jQuery to website:
+//$("h1");
+//S.fn.init [h1#level-title, prevObject: S.fn.init(1)]
