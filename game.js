a4257e171a11f65b8f6b96708305a6eb0c2c0f4a
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-21 19:16:18 -0600

    Update game.js

--- game.js
+++ game.js
@@ -58,16 +58,16 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
   setTimeout(function() {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
 function start() {
